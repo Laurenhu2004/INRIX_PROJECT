70ebc803ce225a5a5a1d3ca9f2fab5bbea3bd1bc
gmeschi <gmeschi@scu.edu>
2023-11-11 18:49:12 -0800

    Initial push

--- data.py
+++ data.py
@@ -8,10 +8,10 @@ location_API = requests.get("https://geocode.maps.co/search?q={}".format(address
 info = location_API.text
 parse_json = json.loads(info)
 lati = parse_json[0]['lat']
-print(lati)
+#print(lati)
 
 long = parse_json[0]['lon']
-print(long)
+#print(long)
 
 url = "https://api.iq.inrix.com/blocks/v3?point={}%7C{}&radius={}".format(lati, long, radius)
 
@@ -19,9 +19,8 @@ url = "https://api.iq.inrix.com/blocks/v3?point={}%7C{}&radius={}".format(lati, 
 
 payload = {}
 headers = {
-  'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcHBJZCI6IndtbGR5bmRzemMiLCJ0b2tlbiI6eyJpdiI6ImQ0NjE3YmNlZDA5ZWQ0MzhmMDRiNzkyN2I4YmU4MDM2IiwiY29udGVudCI6Ijk5N2FhZTk2ODBhMGY0ZTA3Yzg0ZTcwMGEwMTFiMjhjOWZhYzY5OThmZjhhZWRkZGM1ZWU1Y2U3MWZiYTQzNjY2NTkzMjczY2YxMzFhYzk2YmUxMjFlODFmMmEwMWNkNDE0NjczODBmOWU4MjM1OGUxZDg0NTdmZTA1NzJkNDE4ZjdlNjdjOGFmYjM2ZTYwMzk5MTljYzAzNjA2YzMzOWJmM2FkMmJjMDFmNjIzODExYjQzMjM0NjRlMjZhYzhhMzQzN2Q2MGNmNTk1YzNlMjRkNGZjOTkwMjY2NDNmZDRkZTc0Y2NhNTE1NDU3NDQ5ZWExMmZiMTAyYTNmZmFjZDk0Y2RjY2Y3MDU3MjM1ZDNiOWNlYjNkMzBiMzQ2ODc2OTNjZDIxMjU2NzhiMjVmNmVhNzliOGNlYzU4ZTJlNGQwMjczMjYwYzU1ODIyN2Q5ZTc0MjQwNDU2NTY5ZmFjNzg3Y2Y0Nzk4ZDI4NDE2NjE4MTdhMjVmMzMwYmQ5MzYzZDk5OWQ1NTE5Yjc0MjY4NDU5YzM0MTZlZjAzODUzMTQ5OWQyNzQxZDdkN2MzZTRhNjBjOWNjZmZiMjEzNDIyNWNmNDhiYWRhNjhkNGViMjljMGUzYzJiZWIzZGQ3ZTZjYWVkM2M0Yjg4MmMxMjVkN2I4NDI5NWEwZjkyZjdhZjk3ZTgyMzJlYjA2ZjE5YzBlYmRlNTViNmJkZDJiZGUyMTk1ODBiM2E4NWJmZGM4MTEyMWEyOGJjZmU2ZmRhZGY1YTZlMzlkZjVjODIyMTgxYTg1MjkzNTZlOWFlYjg3YjdkOWU4Mzc2ODUxOGVlNmM1ZjI3N2Y5NGQ0MjlhMGRjNDYxNzJkOGY1YWJkNjlkZjg3OWExMjczIn0sInNlY3VyaXR5VG9rZW4iOnsiaXYiOiJkNDYxN2JjZWQwOWVkNDM4ZjA0Yjc5MjdiOGJlODAzNiIsImNvbnRlbnQiOiJkMTZjZGRiYmQ0ZDZlNWNhMWU5Nzg3MGE5MjNhOTZkY2JkODM3MWRiZGY5MGNkODFkNGRlM2ZhMjE4OWM1YjczMDFhNjE3NmRiOTM2OTZiYTgxMjMyNWJmIn0sImp0aSI6ImFhMjYwNDk5LTg4MmQtNDk4MC05MTZhLTAyMThjN2NiYWI5OCIsImlhdCI6MTY5OTc1MzQxMSwiZXhwIjoxNjk5NzU3MDExfQ.JFl9jtRMhYQrFsGYJPRvOwGpXuVEphgx114syrQBzKE'
+  'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcHBJZCI6IndtbGR5bmRzemMiLCJ0b2tlbiI6eyJpdiI6ImZiZDU5ZThiOWU1YzU0ZjU4Nzc1OTA2OWQzZDg2MmVkIiwiY29udGVudCI6ImM5MmYzNTJjYzAxMzJhMGI1Y2Q1NzZkOGRkMDYwYzUzMTZmYjBhMWU1ODg4ZDU5OTEyYTE5ZWVlZDk3M2UyY2Y3MWNiMjFlZDhhZDhkYzdkNzQwZTAyMzdjZTEwMWVmODFmYzBkNzQ2NmQ5MjkwZThjNmZkZDAzNmUzNDMxZjA1YTdlNDBjYmY2ZWI5N2I2ODkxYzg3MDU2NmY1ZTZlNDk5NGJmZTA1NmRmZWU2OGQ0N2E5NjY5NmZjYjc0NTI3OTg4MDdiOWQ0ODJhNWUxZTBiNDUzMWU4ZjYxMWI2YzlhZWYxYjFiZTcyZTI1ZDVlZDFiYTcxOTMwZDViNmNkZTUyNzIxYTMxYzYwMjcwNWI3MGMzNTljMDg2MTg2NDY1ZTRhMzkzZTg3OTVmZGY3MzhlZTU1M2ZmODc5MjYyYTIwMzU2ZThlMTk5NzMzNThlNzE2YzgyYTNiNDdiN2ZiMzZhNmYxNWFkMmE0NWNlZmVjOGEyNDA1NjVlZjM1MjFiMGI0MzY3YzI1ZGNiYWUzYzRmOGJhYzhkMTFhMjQwMTY0OTE2NmFkOWY4YTc5MzljYzZhMDYzNmFkYzc1NWVhNWU2MWQwMmRjYjZlZGE1YmY4MmM5MTRiZDhiOTlhNDE3YTQ2YzM5OWVjMzZmNzQwM2M5OGMwOTJlN2Q4Y2M2OTY4NmM1OWU5ZTVkNzBkZGJiMmUwMTdhOWZmYjY1ZTJjMTgyMTMwNzZjMmExOGEwNjE0ZTNlOGY0ODQyYWNjYmFjY2RjODFmMmVlOTI4OWM4YTdkOWQ4MWQ3OGU1NTkyN2MwYTZhYTkyZjUzNTM3N2I4MzhiNTlhMmFhNDRiZDZmZjI3Y2QzMTVlZDkxNjY2NTMzMTYyMmNkIn0sInNlY3VyaXR5VG9rZW4iOnsiaXYiOiJmYmQ1OWU4YjllNWM1NGY1ODc3NTkwNjlkM2Q4NjJlZCIsImNvbnRlbnQiOiJjZDdjMzgwMTkyMWMwMzVhMmFlODBhZGFjZTE3MTc1NjNkZDMzNzFhNWRiNWY2ZDUxOWI0YTViMWM0NmJmMWYxNmJlZDIzZTdkMGQ4ZTM3NjZmMDQxMzA5In0sImp0aSI6IjIyNWIxZjdjLTRiNjEtNDE0My05MzNkLThlYmI4YTk1NGJmYSIsImlhdCI6MTY5OTc1NTgwMiwiZXhwIjoxNjk5NzU5NDAyfQ.O6_o6Vr89yrvhHXgv2ZrPQpyjbjVJz6Y2HOzEyOBumg'
 }
-
 response = requests.request("GET", url, headers=headers, data=payload)
 
 #print(response.text)
@@ -31,8 +30,10 @@ probability = []
 openSegments = []
 segmentNames = []
 segmentDict = {}
+probDict = {}
 
 r = (response.json()).get('result')
+#print(r)
 
 for i in range(0, len(r)):
     names.append(r[i].get('name'))
@@ -44,25 +45,22 @@ for i in range(0, len(r)):
         isOpen = segments[j].get('isOpen')
 
         if spaces >= 1 & isOpen == True:
-            #openSegments[r[i].get('name')] = segments[j].get('segmentID')
             openSegments.append(segments[j].get('segmentID'))
             segmentNames.append(r[i].get('name'))
 
+        
 
-for k in range(len(openSegments)):
-    segmentDict[openSegments[k]] = segmentNames[k]
 
+for k in range(0, len(openSegments)):
+    if 
+        temp = [segmentNames[l], probability[k]]
+        segmentDict[openSegments[l]] = temp
 
     
-
 #print(names)
-print(probability)
+#print(probability)
 #print(openSegments)
 #print(segmentNames)
 
+#print(probDict)
 print(segmentDict)
-print(len(segmentDict))
-
-#r = ((response.json()).get('result'))[0].get('name')
-
-#print(r)
